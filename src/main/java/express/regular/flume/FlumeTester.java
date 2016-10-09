d5055358decf218d4dab74af760f88cd918427f3
bdh92123 <bdh92123.1@gmail.com>
2016-10-10 01:06:42 +0900

    Initial Commit

---
+++ src/main/java/express/regular/flume/FlumeTester.java
@@ -0,0 +1,120 @@
+package express.regular.flume;
+
+import com.google.gson.Gson;
+import express.regular.common.*;
+import express.regular.exception.InvalidConfigException;
+import org.apache.flume.Context;
+import org.apache.flume.Event;
+import org.apache.flume.event.EventBuilder;
+import org.apache.flume.interceptor.Interceptor;
+import org.apache.flume.interceptor.RegexExtractorInterceptor;
+import org.apache.flume.interceptor.RegexFilteringInterceptor;
+
+import java.io.*;
+import java.util.*;
+
+public class FlumeTester extends Tester {
+
+    public static final String CONFIG_TYPE = "test_type";
+    public static final String CONFIG_FILTERING_REGEX = RegexFilteringInterceptor.Constants.REGEX;
+    public static final String CONFIG_FILTERING_EXCLUDE_EVENTS = RegexFilteringInterceptor.Constants.EXCLUDE_EVENTS;
+    public static final String CONFIG_EXTRACTOR_FLUME_CONTEXT = "flume_context";
+
+    public static final String TYPE_FILTERING = "filtering";
+    public static final String TYPE_EXTRACTOR = "extractor";
+
+    private static Gson gson = new Gson();
+
+    public TestResult testRegexFilteringInterceptor(Map<String, Object> configMap, List<String> testStrings) throws IOException {
+        TestResult testResult = new TestResult();
+        testResult.setType(TestResult.Type.MATCH);
+
+        Context context = new Context();
+        if(configMap.containsKey(CONFIG_FILTERING_REGEX)) {
+            context.put(RegexFilteringInterceptor.Constants.REGEX, (String) configMap.get(CONFIG_FILTERING_REGEX));
+        }
+        if(configMap.containsKey(CONFIG_FILTERING_EXCLUDE_EVENTS)) {
+            context.put(RegexFilteringInterceptor.Constants.EXCLUDE_EVENTS, (String) configMap.get(CONFIG_FILTERING_EXCLUDE_EVENTS));
+        }
+
+        RegexFilteringInterceptor.Builder regexFilteringInterceptor = new RegexFilteringInterceptor.Builder();
+        regexFilteringInterceptor.configure(context);
+        Interceptor interceptor = regexFilteringInterceptor.build();
+        interceptor.initialize();
+
+        MatchResult matchResult = new MatchResult();
+        for(int i = 0; i < testStrings.size(); i++) {
+            String testString = testStrings.get(i);
+            Event event = EventBuilder.withBody(testString.getBytes());
+            event = interceptor.intercept(event);
+            if(event != null) {
+                matchResult.getResultList().add(true);
+            } else {
+                matchResult.getResultList().add(false);
+            }
+        }
+        testResult.setResult(matchResult);
+
+        return testResult;
+    }
+
+    public TestResult testRegexExtractorInterceptor(Map<String, Object> configMap, List<String> testStrings) throws IOException {
+        TestResult testResult = new TestResult();
+        testResult.setType(TestResult.Type.GROUP);
+
+        String flumeContextString = (String) configMap.get(CONFIG_EXTRACTOR_FLUME_CONTEXT);
+        Properties props = new Properties();
+        props.load(new StringReader(flumeContextString));
+
+        Context context = new Context((Map)props);
+        RegexExtractorInterceptor.Builder builder = new RegexExtractorInterceptor.Builder();
+        builder.configure(context);
+        Interceptor interceptor = builder.build();
+
+        GroupResult groupResult = new GroupResult();
+        for(String testString : testStrings) {
+            Event event = EventBuilder.withBody(testString.getBytes());
+            event.setHeaders(new LinkedHashMap<String, String>());
+            event = interceptor.intercept(event);
+            int group = 0;
+            List<Object> row = new ArrayList(event.getHeaders().size());
+            for(Map.Entry<String, String> entry : event.getHeaders().entrySet()) {
+                String headerName = entry.getKey();
+                String value = entry.getValue();
+                row.add(value);
+
+                if(group == 0) {
+                    groupResult.getColumns().add(headerName);
+                }
+            }
+            if(row.size() > 0) {
+                groupResult.getResultList().add(row);
+            } else {
+                groupResult.getResultList().add(null);
+            }
+        }
+
+        testResult.setResult(groupResult);
+        return testResult;
+    }
+
+    public TestResult testRegex(Map<String, Object> configMap, List<String> testStrings) throws Exception {
+        String testType = (String) configMap.get(CONFIG_TYPE);
+        if(testType == null) {
+            throw new InvalidConfigException(String.format("'%s' parameter doesn't exists.", CONFIG_TYPE));
+        }
+
+        if (testType.equals(TYPE_FILTERING)) {
+            return testRegexFilteringInterceptor(configMap, testStrings);
+        } else if (testType.equals(TYPE_EXTRACTOR)) {
+            return testRegexExtractorInterceptor(configMap, testStrings);
+        } else {
+            throw new InvalidConfigException(String.format("Unsupported Test Type: %s", testType));
+        }
+    }
+
+    public static void main(String args[]) {
+        Tester tester = new FlumeTester();
+        tester.testMain(args[0], args[1]);
+    }
+}
---
+++ src/test/java/express/regular/flume/test/FlumeTesterTest.java
@@ -0,0 +1,75 @@
+package express.regular.flume.test;
+
+import com.google.gson.Gson;
+import express.regular.common.GroupResult;
+import express.regular.common.MatchResult;
+import express.regular.common.TestResult;
+import express.regular.common.Tester;
+import express.regular.flume.FlumeTester;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FlumeTesterTest {
+
+    @Test
+    public void flumeFilteringTest() {
+        Map<String, Object> configMap = new HashMap<String, Object>();
+        configMap.put(Tester.CONFIG_IS_DEBUG, true);
+        configMap.put(FlumeTester.CONFIG_TYPE, FlumeTester.TYPE_FILTERING);
+        configMap.put(FlumeTester.CONFIG_FILTERING_REGEX,  "([a-zA-Z]*) ([a-zA-Z]*) ([a-zA-Z]*)");
+        configMap.put(FlumeTester.CONFIG_FILTERING_EXCLUDE_EVENTS, "false");
+
+        List<String> testMap = Arrays.asList(new String[]{"Hello Test String", "Hello2 Test2 String2"});
+
+        Gson gson = new Gson();
+        String configJsonString = gson.toJson(configMap);
+        String testJsonString = gson.toJson(testMap);
+
+        FlumeTester flumeTester = new FlumeTester();
+        TestResult testResult = flumeTester.testMain(configJsonString, testJsonString);
+        Assert.assertEquals(testResult.getType(), TestResult.Type.MATCH);
+        Assert.assertNotNull(testResult.getResult());
+        MatchResult matchResult = (MatchResult) testResult.getResult();
+        Assert.assertEquals(matchResult.getResultList().get(0), true);
+        Assert.assertEquals(matchResult.getResultList().get(1), false);
+    }
+
+    @Test
+    public void flumeExtractorTest() {
+        Map<String, Object> configMap = new HashMap<String, Object>();
+        configMap.put(Tester.CONFIG_IS_DEBUG, true);
+        configMap.put(FlumeTester.CONFIG_TYPE, FlumeTester.TYPE_EXTRACTOR);
+        StringBuffer flumeContext = new StringBuffer();
+        flumeContext.append("regex=([a-zA-Z]*) ([a-zA-Z]*) ([a-zA-Z]*)").append("\n")
+                .append("serializers=s1 s2 s3").append("\n")
+                .append("serializers.s1.name=A").append("\n")
+                .append("serializers.s1.type=DEFAULT").append("\n")
+                .append("serializers.s2.name=B").append("\n")
+                .append("serializers.s2.type=DEFAULT").append("\n")
+                .append("serializers.s3.name=C").append("\n")
+                .append("serializers.s3.type=DEFAULT").append("\n");
+
+        configMap.put(FlumeTester.CONFIG_EXTRACTOR_FLUME_CONTEXT, flumeContext.toString());
+
+        List<String> testStrings = Arrays.asList(new String[]{"Hello Test String", "Hello2 Test2 String2"});
+
+        Gson gson = new Gson();
+        String configJsonString = gson.toJson(configMap);
+        String testJsonString = gson.toJson(testStrings);
+
+        FlumeTester flumeTester = new FlumeTester();
+        TestResult testResult = flumeTester.testMain(configJsonString, testJsonString);
+
+        Assert.assertEquals(testResult.getType(), TestResult.Type.GROUP);
+        Assert.assertNotNull(testResult.getResult());
+        GroupResult groupResult = (GroupResult) testResult.getResult();
+        Assert.assertArrayEquals(groupResult.getColumns().toArray(), new String[]{"A", "B", "C"});
+        Assert.assertArrayEquals(groupResult.getResultList().get(0).toArray(), new String[]{"Hello", "Test", "String"});
+        Assert.assertNull(groupResult.getResultList().get(1));
+    }
+}
